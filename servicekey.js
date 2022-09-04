8e8c2e55a5dcdc585402fc9aa887c161e66c6174
ak <akshay21kh@gmail.com>
2022-09-04 16:23:13 +0530

    second commit

--- servicekey.js
+++ servicekey.js
@@ -6,13 +6,13 @@ require("firebase/firestore");
 
 
 var firebaseConfig = {
-  apiKey: "AIzaSyB3TxST1bFADsUJa7L4IHtYV86jkSr07LM",
-  authDomain: "expressfirebase-fc673.firebaseapp.com",
-  projectId: "expressfirebase-fc673",
-  storageBucket: "expressfirebase-fc673.appspot.com",
-  messagingSenderId: "1016975049147",
-  appId: "1:1016975049147:web:cf1d2f83abb76cf101c49e",
-  measurementId: "G-JWJR37FH5P"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
